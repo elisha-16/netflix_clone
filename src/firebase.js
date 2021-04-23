5387b5fd76b3a8664484a4a18ff5bcab010eae82
elisha-16 <ghoshelisha19@gmail.com>
2021-04-23 13:05:04 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrN3w_vZ6v7fBT8Br5e3HRa0iJCKKiNa8",
+    authDomain: "netflix-clone-24ad3.firebaseapp.com",
+    projectId: "netflix-clone-24ad3",
+    storageBucket: "netflix-clone-24ad3.appspot.com",
+    messagingSenderId: "91795276531",
+    appId: "1:91795276531:web:d460acf1e1e1ea979c6d86"
+  };
